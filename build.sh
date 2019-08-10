b36a06b38181114ae0a10250b4654a9f0c536d14
Gary Kim <gary@garykim.dev>
2019-08-10 19:40:30 +0800

    Add Accidently Removed OSS Tag
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
+go build --tags oss -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
