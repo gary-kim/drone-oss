685dcef87852a3f950dcbb5ceaead5c219bd12b2
Gary Kim <gary@garykim.dev>
2019-09-17 19:41:54 +0800

    Update build script with nolimit tag

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build --tags oss -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
+go build --tags "oss nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
