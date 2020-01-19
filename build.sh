81884d0867ad01070e4e022cf77f793df7075b6b
Gary Kim <gary@garykim.dev>
2020-01-19 21:52:38 +0800

    Add personal Drone nolimit image

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ "$1" == "nolimit" ]]; then
+    go build --tags "nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
+    exit 0
+fi
 
 go build --tags "oss nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
