82d3257557cddc9ad3ee6609fc2677d8a2b6846d
Thomas Daley <tomdaley92@gmail.com>
2021-06-19 19:00:47 -0700

    test tags argument

--- tags.sh
+++ tags.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo -n "latest$1,`git describe --tags | grep -ioP '\d+.\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+' | head -1`$1"
+echo -n "latest-$1,`git describe --tags | grep -ioP '\d+.\d+.\d+' | head -1`-$1,`git describe --tags | grep -ioP '\d+.\d+' | head -1`-$1,`git describe --tags | grep -ioP '\d+' | head -1`-$1"
